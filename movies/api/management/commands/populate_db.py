0cb284372f3f82cd4fcf41fb2d9904abf3cf2ec3
Swapnil Sawant <sawant.swapnil1@gmail.com>
2020-07-31 14:49:39 +0530

    changed custom command populate db for heroku config

--- movies/api/management/commands/populate_db.py
+++ movies/api/management/commands/populate_db.py
@@ -16,29 +16,37 @@ class Command(BaseCommand):
         save movie with genres.
 
         """
+        try:
 
-        json_file = f'{settings.BASE_DIR}/../imdb.json'
+            json_file = f'{settings.BASE_DIR}/../imdb.json'
 
-        with open(json_file) as file:
-            # if file is to big for memory we can then use pandas
-            # currently this works for now
-            movies_data = file.read()
-            movies_list = json.loads(movies_data)
-            # creating generators for fast and memory efficient processing
-            movies_generator = (movie for movie in movies_list)
-            single_movie = {}
-            for movie_obj in movies_generator:
-                single_movie['popularity'] = movie_obj.get('99popularity')
-                single_movie['director'] = movie_obj.get('director')
-                single_movie['imdb_score'] = movie_obj.get('imdb_score')
-                single_movie['name'] = movie_obj.get('name')
+            with open(json_file) as file:
+                # if file is to big for memory we can then use pandas
+                # currently this works for now
+                movies_data = file.read()
+                movies_list = json.loads(movies_data)
+                # creating generators for fast and memory efficient processing
+                movies_generator = (movie for movie in movies_list)
+                single_movie = {}
+                for movie_obj in movies_generator:
+                    single_movie['popularity'] = movie_obj.get('99popularity')
+                    single_movie['director'] = movie_obj.get('director')
+                    single_movie['imdb_score'] = movie_obj.get('imdb_score')
+                    single_movie['name'] = movie_obj.get('name')
 
-                movie, created = Movie.objects.get_or_create(**single_movie)
-                genres = movie_obj.get('genre')
+                    movie, created = Movie.objects.get_or_create(**single_movie)
+                    genres = movie_obj.get('genre')
 
-                # create and link genres to movies
-                for genre in genres:
-                    name = genre.strip()
-                    genre, created = Genre.objects.get_or_create(name=name)
-                    movie.genre.add(genre)
-                movie.save()
+                    # create and link genres to movies
+                    for genre in genres:
+                        name = genre.strip()
+                        genre, created = Genre.objects.get_or_create(name=name)
+                        movie.genre.add(genre)
+                    movie.save()
+
+            self.stdout.write(self.style.SUCCESS('Successfully populated the database'))
+            return
+
+        except IOError as e:
+            self.stdout.write(self.style.ERROR(f'File not found. {e}'))
+            return
