a4a697ad760226668b92175f41dc241f3067a66c
Swapnil Sawant <sawant.swapnil1@gmail.com>
2020-07-31 21:37:49 +0530

    added test case for populate database custom django admin command

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from io import StringIO
+from django.core.management import call_command
 
 # Create your tests here.
+
+
+class PopulateDBTest(TestCase):
+    """
+    Unit test case for custom management command to populate database.
+
+    """
+
+    def test_command_output(self):
+        out = StringIO()
+        call_command('populate_db', stdout=out)
+        self.assertIn('Successfully populated the database', out.getvalue().strip())
