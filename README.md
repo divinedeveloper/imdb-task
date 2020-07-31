bde51dd470bb270ed3eb3574288a34fb6c837d8c
Swapnil Sawant <sawant.swapnil1@gmail.com>
2020-07-31 05:51:01 +0530

    added custom command to poppulate db

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # imdb-task
 RESTful API for movies, something similar to IMDB
+
+#Requirements 
+Python 3.6
+Django 3.0.7
+
+#CLone the directory to desired folder
+cd imdb-task
+
+#Create virtual environment and activate it
+virtualenv -p python3 env
+source env/bin/activate
+
+#Pip install requirements
+pip install -r requirements.txt
+
+cd movies/
+
+#Run Migrations
+python manage.py makemigrations
+python manage.py migrate
+
+#Populate the database by running following custom django admin command
+python manage.py populate_db
---
+++ movies/api/management/commands/populate_db.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.models import Movie, Genre
+from django.conf import settings
+import json
+
+
+class Command(BaseCommand):
+    help = 'Populates the database from json file'
+
+    def handle(self, *args, **kwargs):
+        """
+        Reads json file and loads in json
+        create a generator to process fast and memory efficient huge movies data
+        get or create a single movie object
+        get or create a genre and link to movie
+        save movie with genres.
+
+        """
+
+        json_file = f'{settings.BASE_DIR}/../imdb.json'
+
+        with open(json_file) as file:
+            # if file is to big for memory we can then use pandas
+            # currently this works for now
+            movies_data = file.read()
+            movies_list = json.loads(movies_data)
+            # creating generators for fast and memory efficient processing
+            movies_generator = (movie for movie in movies_list)
+            single_movie = {}
+            for movie_obj in movies_generator:
+                single_movie['popularity'] = movie_obj.get('99popularity')
+                single_movie['director'] = movie_obj.get('director')
+                single_movie['imdb_score'] = movie_obj.get('imdb_score')
+                single_movie['name'] = movie_obj.get('name')
+
+                movie, created = Movie.objects.get_or_create(**single_movie)
+                genres = movie_obj.get('genre')
+
+                # create and link genres to movies
+                for genre in genres:
+                    name = genre.strip()
+                    genre, created = Genre.objects.get_or_create(name=name)
+                    movie.genre.add(genre)
+                movie.save()
