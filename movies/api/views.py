89f36e97f86833d161b9ac67e664743d93494a22
Swapnil Sawant <sawant.swapnil1@gmail.com>
2020-07-31 08:39:45 +0530

    removed unsued imports

--- movies/api/views.py
+++ movies/api/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework import filters
 from api.models import Movie
